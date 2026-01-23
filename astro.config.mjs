a88956bd772f939c0822bc72af75f8e4977b0c59
Kousuke Yoneta <yoneta@valuesccg.com>
2026-01-23 10:08:45 +0900

    update terms page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://kanaeru-tools.com",
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
 });
